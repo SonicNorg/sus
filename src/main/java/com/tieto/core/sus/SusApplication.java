e68a7f1e2050775d43e2f3f21457f8c75e37bcfa
Axel Kislin <alexander.kislin@tieto.com>
2020-07-27 18:18:26 +0300

    settings, flyway migration, consul kv, first run.

--- src/main/java/com/tieto/core/sus/SusApplication.java
+++ src/main/java/com/tieto/core/sus/SusApplication.java
@@ -2,8 +2,14 @@ package com.tieto.core.sus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableRetry
+@EnableConfigurationProperties
 public class SusApplication {
 
 	public static void main(String[] args) {
