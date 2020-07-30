1c3b775e3e0897e3ac1bf1d4ad1c89a07e72e442
Axel Kislin <alexander.kislin@tieto.com>
2020-07-30 22:52:41 +0300

    fixed db structure, and logs config.

--- app/src/main/resources/db/migration/V0.1.1__initial.sql
+++ app/src/main/resources/db/migration/V0.1.1__initial.sql
@@ -1,20 +1,5 @@
 CREATE TABLE LSMSISDN (
-id number not null constraint lsmsisdn_pk primary key,
 msisdn varchar(10) not null constraint msisdn_uc unique,
-accountId varchar(8) not null constraint accountId_uc unique,
+accountId varchar(8) not null constraint lsmsisdn_pk primary key,
 status varchar(255) not null
 );
-
-create sequence
- lsmsisdn_pk_seq
-increment by 1
-start with 1;
-
-CREATE TRIGGER lsmsisdn_before
-BEFORE INSERT ON LSMSISDN
-FOR EACH ROW
-BEGIN
-SELECT lsmsisdn_pk_seq.nextval
-INTO :new.id
-FROM dual;
-END;
